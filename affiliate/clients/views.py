3fcbcec6fcc678a15950de4ddb68cb9164345657
Richmond Kessie <kessierich.mag@gmail.com>
2022-11-05 01:20:35 +0000

    EBEFA

--- affiliate/clients/views.py
+++ affiliate/clients/views.py
@@ -1,7 +1,12 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Members
 
 # Create your views here.
 def index(request):
+    myclients = Members.objects.all().values()
     template = loader.get_template('clients.html')
-    return HttpResponse(template.render())
+    context = {
+        'myclients': myclients,
+    }
+    return HttpResponse(template.render(context, request))
